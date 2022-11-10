4478e5ae5ac2a7e8a1014176624e62362fb1bbeb
Parantapa Bhattacharya <pb@parantapa.net>
2022-11-10 14:10:31 -0500

    Update README with an example.

--- examples/example1.sql
+++ examples/example1.sql
@@ -1,6 +1,19 @@
-# This is an exmaple of sqlpygen file
+# example1.sql
+# This is an example of annotated sql file.
+
+# Lines starting with # are ignored by SqlPyGen.
+# Lines starting with -- are used to provide SqlPyGen with annotations.
+
+# Using the "module" annotation
+# we name the python module/file to be generated.
+# This will create a file called example1.py
 -- module: example1
 
+# We use "schema" annotations to annotate
+# create table and create index sql statements.
+# The statements are given a name which is used in the generated code
+# to produce better error messages.
+# Each SQL statement must end with a semicolon.
 -- schema: table_stocks
 
 CREATE TABLE stocks (
@@ -11,11 +24,28 @@ CREATE TABLE stocks (
     price real
 ) ;
 
+# Here we annotate a query.
+# The name of the query is used as the name of the function that is generated.
+# The query parameters are named and annotated with types.
+# The names can then be used in the query.
+# Since this query doesn't specify a return annotation,
+# SqlPyGen assumes that this SQL statement doesn't return anything.
+# The types here are builtin Python types.
+# By marking types ! at their end, we inform SqlPyGen,
+# that the specific parameter may never by None.
 -- query: insert_into_stocks
--- params: date: str, trans: str, symbol: str, qty: float, price: float
+-- params: date: str!, trans: str!, symbol: str!, qty: float, price: float
 
 INSERT INTO stocks VALUES (:date, :trans, :symbol, :qty, :price) ;
 
+# Here we annotate a query with no parameters
+# but one that returns some columns.
+# Based the name of the query, and the return annotation
+# SqlPyGen will generate a Python dataclass of appropriate type.
+# The "return*" annotation tells SqlPyGen that this query may return
+# zero or more rows.
+# The following query is annotated with "return?",
+# tells SqlPyGen that it will return zero or exactly one row.
 -- query: select_from_stocks
 -- return*: date: str, trans: str, symbol: str, qty: float, price: float
 
